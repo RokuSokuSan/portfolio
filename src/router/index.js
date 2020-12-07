de892570f5680fd93719feda12027ec227a41c07
Anthony Norman <anthnorm@gmail.com>
2020-12-07 15:31:33 +0100

    Sec commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import BlogView from '../views/BlogView.vue'
 import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 
 
@@ -23,17 +24,17 @@ const routes = [
   },
   {
     path: '/Login',
-    name: 'Login',
+    name: 'login',
     component: Login
   },
   {
-    path: '/BlogView',
-    name: 'BlogView',
-    component: BlogView
+    path: '/Register',
+    name: 'register',
+    component: Register
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
