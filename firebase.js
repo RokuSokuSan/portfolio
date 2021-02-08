1c3960a1e3c732e827f3b53fabecb36ef90de589
Anthony Norman <anthnorm@gmail.com>
2021-02-08 15:05:08 +0100

    Push test 7

--- firebase.js
+++ firebase.js
@@ -5,13 +5,23 @@ import 'firebase/firestore'
   // Initialize Specific Data Store (Firebase - Firestore)
  // Your web app's Firebase configuration
  const firebaseConfig = {
-  apiKey: "AIzaSyCiqyc_q79aWyRokki38pc4ByB-1nW8iZs",
-  authDomain: "anth-portfolio.firebaseapp.com",
-  projectId: "anth-portfolio",
-  storageBucket: "anth-portfolio.appspot.com",
-  messagingSenderId: "191884125980",
-  appId: "1:191884125980:web:262002fc932dcc4df67cd9"
-};
+    apiKey: "AIzaSyC_pWGPlc8r9Vx2V89cq9EUc16CWtmPZvU",
+    authDomain: "portfolio-anth.firebaseapp.com",
+    projectId: "portfolio-anth",
+    storageBucket: "portfolio-anth.appspot.com",
+    messagingSenderId: "729321349205",
+    appId: "1:729321349205:web:d6747d9e832d4fdda7b4dc",
+    measurementId: "G-LRREK59MR7"
+  };
+
+//firebaseConfig = {
+//   apiKey: "AIzaSyCiqyc_q79aWyRokki38pc4ByB-1nW8iZs",
+//   authDomain: "anth-portfolio.firebaseapp.com",
+//   projectId: "anth-portfolio",
+//   storageBucket: "anth-portfolio.appspot.com",
+//   messagingSenderId: "191884125980",
+//   appId: "1:191884125980:web:262002fc932dcc4df67cd9"
+// };
 
   // const firebaseConfig = {
   //     apiKey: "AIzaSyDGh1cfvCjmIw_uUeKWMi0Ieq9W6K673LI",
